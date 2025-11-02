27825dd48db2c8d62dd45f1ca5ed5e06a5c741e0
Theo Müller <theopaul2001@gmx.de>
2025-11-02 16:47:38 +0100

    magit configit support waybar styling

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -116,3 +116,24 @@
 (map! :leader
       :desc "Diff buffer with file"
       "b d" #'buffer-diff)
+
+
+
+(defun my/magit-process-environment (env)
+  "Detect and set git -bare repo env vars when in tracked dotfile directories."
+  (let* ((default (file-name-as-directory (expand-file-name default-directory)))
+         (git-dir (expand-file-name "~/.cfg/"))
+         (work-tree (expand-file-name "~/"))
+         (dotfile-dirs
+          (-map (apply-partially 'concat work-tree)
+                (-uniq (-keep #'file-name-directory (split-string (shell-command-to-string
+                (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
+                        git-dir work-tree))))))))
+    (push work-tree dotfile-dirs)
+    (when (member default dotfile-dirs)
+      (push (format "GIT_WORK_TREE=%s" work-tree) env)
+      (push (format "GIT_DIR=%s" git-dir) env)))
+  env)
+
+(advice-add 'magit-process-environment
+            :filter-return #'my/magit-process-environment)
